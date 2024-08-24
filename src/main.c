e0cc98c912184b870800871f3aacdee5f928dd15
Bella5968 <abitimenge@gmail.com>
2024-08-23 23:42:32 -0700

    task 0

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point for the simple shell program.
+ *
+ * Return: Always returns 0 (Success).
+ */
+int main(void)
+{
+printf("Welcome to the Simple Shell!\n");
+return (0);
+}
+
